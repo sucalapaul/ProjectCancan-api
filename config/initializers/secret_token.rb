bfb23d32e943764306cdeb387dc7eaf4b0b29be3
Paul Sucala <sucala.paul@gmail.com>
2012-11-14 12:49:27 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectCancanApi::Application.config.secret_token = '88800514cb198c86099daf82500ebf32860c99cfa79245c38f03a1eac99f32aa19e7cd45f1043cbfd2c8df2e00d4459adf9b03a3a044348f1170f5aea50b0548'
